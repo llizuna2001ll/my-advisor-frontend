367f111c56e131b91c81e214d2492ac700e16bf6
llizuna2001ll <issam.fladi@gmail.com>
2023-05-09 11:09:25 +0100

    dynamic cities

--- src/pages/AllCitiesPage.js
+++ src/pages/AllCitiesPage.js
@@ -1,30 +1,40 @@
 import {Link} from "react-router-dom";
 import Navigation from "../components/Navigation";
 import '../css/allcities.css';
+import {useState} from "react";
 function AllCitiesPage() {
+
+    const token = localStorage.getItem('token');
+    const [cities, setCities] = useState([]);
+
+    fetch('http://localhost:8888/api/v1/cities', {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            setCities(data);
+            console.log(cities);
+        })
+        .catch(error => console.error(error));
+
     return(
         <>
+
+
+
             <Navigation/>
             <div className=" container mt-5">
                 <h2 className="fw-bold">All cities</h2>
-                <div style={{marginLeft:"150px"}} className="row justify-content-center mb-2 mt-3">
-                    <div className="card allcities-container">
-                        <Link style={{fontSize:"30px", textDecoration:"none"}} to="/cities/marrakesh"><div  className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src="./images/cities/kech.jpg"/> Marakkesh</div></Link>
-                    </div>
-                    <div  className="card allcities-container">
-                        <Link style={{fontSize:"30px", textDecoration:"none"}} to="/cities/casablanca"><div style={{fontSize:"30px"}} className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src="./images/cities/casablanca.jpg"/> Casablanca</div></Link>
-                    </div>
-                    <div  className="card allcities-container">
-                        <Link style={{fontSize:"30px", textDecoration:"none"}} to="/cities/tangier"><div style={{fontSize:"30px"}} className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src="./images/cities/tangier.jpg"/> Tangier</div></Link>
-                    </div>
-                    <div  className="card allcities-container">
-                        <Link style={{fontSize:"30px", textDecoration:"none"}} to="/cities/fes"><div style={{fontSize:"30px"}} className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src="./images/cities/fes.jpg"/> Fez</div></Link>
-                    </div>
-                    <div  className="card allcities-container">
-                        <Link style={{fontSize:"30px", textDecoration:"none"}} to="/cities/agadir"><div style={{fontSize:"30px"}} className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src="./images/cities/agadir.jpg"/> Agadir</div></Link>
-                    </div>
-                    <div className="card allcities-container">
-                        <Link style={{fontSize:"30px", textDecoration:"none"}} to="/cities/marrakesh"><div style={{fontSize:"30px"}} className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src="./images/cities/kech.jpg"/> Agadir</div></Link>
+                <div className=" container mt-5">
+                    <div style={{marginLeft:"150px"}} className="row justify-content-center mb-2 mt-3">
+                    {cities.map((city, index)=>
+                            <div className="card allcities-container">
+                                <Link style={{fontSize:"30px", textDecoration:"none"}} to={`/cities/${city.name.toLowerCase()}`}><div  className="card-body"><img style={{opacity: "100%"}} className="topcities-img" height="100px" width="20%" src={city.imgPath}/> {city.name}</div></Link>
+                            </div>
+                )}
                     </div>
                 </div>
             </div>
