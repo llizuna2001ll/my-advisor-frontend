1cbd4b7b09c4517c61a9d7df048fcce378d8b33c
llizuna2001ll <80908480+llizuna2001ll@users.noreply.github.com>
2023-09-08 05:16:15 +0100

    Update CoverComponent.js

--- src/components/CoverComponent.js
+++ src/components/CoverComponent.js
@@ -42,8 +42,8 @@ function CoverComponent() {
     useEffect(() => {
 
         AWS.config.update({
-            accessKeyId: 'AKIAQ4ELPGT7UYVJS7XZ',
-            secretAccessKey: '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',
+            accessKeyId: '**********************************',
+            secretAccessKey: '************************************',
             region: 'eu-north-1',
         });
 
