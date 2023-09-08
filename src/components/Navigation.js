fa9730c8dd6ded695bcfe173d25ec8d1a6a5371e
llizuna2001ll <80908480+llizuna2001ll@users.noreply.github.com>
2023-09-08 05:15:31 +0100

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -73,8 +73,8 @@ function Navigation() {
     useEffect(() => {
 
         AWS.config.update({
-            accessKeyId: 'AKIAQ4ELPGT7UYVJS7XZ',
-            secretAccessKey: '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',
+            accessKeyId: '************************',
+            secretAccessKey: '**************************',
             region: 'eu-north-1',
         });
 
