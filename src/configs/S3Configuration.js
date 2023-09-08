174da728f11380a7a03c025073b3036eb1cddbe6
llizuna2001ll <80908480+llizuna2001ll@users.noreply.github.com>
2023-09-08 05:15:48 +0100

    Update S3Configuration.js

--- src/configs/S3Configuration.js
+++ src/configs/S3Configuration.js
@@ -2,8 +2,8 @@ const AWS = require('aws-sdk');
 
 function S3Configuration() {
     AWS.config.update({
-        accessKeyId: 'AKIAQ4ELPGT7UYVJS7XZ',
-        secretAccessKey: '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',
+        accessKeyId: '***********************************',
+        secretAccessKey: '******************************',
         region: 'eu-north-1',
     });
 }
